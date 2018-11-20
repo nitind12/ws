b65190d5cb37e92d1f8785f7f18c2567cca5670e
DheeruDheeru <44924362+DheeruDheeru@users.noreply.github.com>
2018-11-20 20:11:50 +0530

    add category.php
    category php file

---
+++ add_ctgry.php
@@ -0,0 +1,19 @@
+<?php
+  $cat_name = $_POST['catname'];
+  $cat_desc = $_POST['catdesc'];
+
+  require 'db.php';
+
+  $query=mysqli_query($con,"SELECT * from category where category_name='$cat_name'");
+  $row=mysqli_fetch_assoc($query);
+ 
+  if(mysqli_num_rows($query)!=0)
+  {
+    $bool_ = array('res'=>false, 'record'=>'category already exist.');
+  }
+  else {
+    $res = $con->query("INSERT INTO category(category_name,desc_)
+    values
+    ('$cat_name','$cat_desc')");
+  }
+?>
